684d9f3721070ea253bfdc6e5bcfd0a368db6536
DominikAngerer <me@dominikangerer.com>
2018-06-13 21:17:58 +0200

    Prepare for Auth0
    Adds Callback Route & View
    Enables History Mode

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
+import Callback from "./views/Callback.vue";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history", // enable history mode
   routes: [
     {
       path: "/",
@@ -16,6 +18,11 @@ export default new Router({
       path: "/about",
       name: "about",
       component: About
+    },
+    {
+      path: "/callback",
+      name: "callback",
+      component: Callback
     }
   ]
 });
