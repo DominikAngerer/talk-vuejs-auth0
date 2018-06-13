cf4a038bd9e440a28b5df4d8fdecc46d913ba213
DominikAngerer <me@dominikangerer.com>
2018-06-13 21:23:04 +0200

    Adding Auth0
    Adding the Auth0 Vue Component

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+
+// import Auth & add tell Vue to use it.
+import auth from "./auth";
+Vue.use(auth);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount("#app");
